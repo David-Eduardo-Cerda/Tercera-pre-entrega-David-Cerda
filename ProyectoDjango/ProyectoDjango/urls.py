37c0dfbdbe5798cdf800a9b095a6f2516b3380cd
DavidCerda <david.celular@outlook.com>
2023-04-11 22:37:48 -0300

    Cargando un temple

--- ProyectoDjango/ProyectoDjango/urls.py
+++ ProyectoDjango/ProyectoDjango/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ProyectoDjango.view import *
+from ProyectoDjango.view import ProbandoTemplate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path()
+    path('ProbandoTemplate/', ProbandoTemplate),
 ]

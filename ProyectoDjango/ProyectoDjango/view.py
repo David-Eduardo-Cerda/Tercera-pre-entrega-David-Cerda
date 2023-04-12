436e663a6cc75dd4824171af6b61fcbc20f74c6c
DavidCerda <david.celular@outlook.com>
2023-04-12 19:14:05 -0300

    Temple Funcionando

--- ProyectoDjango/ProyectoDjango/view.py
+++ ProyectoDjango/ProyectoDjango/view.py
@@ -1,19 +1,19 @@
-from django.http import HttpRequest
-from django.template import Template, context
+from django.http import HttpRequest , HttpResponse
+from django.template import Template, Context
 from django.template import loader
 
 def ProbandoTemplate(request):
-
-    diccionario = {"nombre":"David","apellido":"Cerda","edad":32}
-
-    archivo = open(r"C:\Users\David\Desktop\Tercera-pre-entrega-David-Cerda\ProyectoDjango\plantilla\template1.html")
-
-    texto = archivo.read()
-    archivo.close()
-
-    templatE = Template(texto)
-    contexto = context(diccionario)
-
-    documento = Template.render(texto)
     
-    return HttpRequest(documento)
+    diccionario = {"nombre" : "david" , "apellido" : "cerda", "edad" : 33}
+
+
+    archivo = open("C:/Users/David/Desktop/Tercera-pre-entrega-David-Cerda/ProyectoDjango/Plantilla/template1.html")
+
+
+    template = Template(archivo.read())
+    archivo.close()
+    contexto = Context(diccionario)
+
+    documento = template.render(contexto)
+
+    return HttpResponse(documento)
